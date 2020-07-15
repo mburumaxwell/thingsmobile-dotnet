34986fa40594836875731ec7a85ef072feb94dbb
Maxwell Weru <mburumaxwell@gmail.com>
2020-07-15 17:50:52 +0300

    Move sample responses to a samples folder instead of the tests file

---
+++ test/ThingsMobile.Tests/TestSamples.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace ThingsMobile.Tests
+{
+    internal static class TestSamples
+    {
+        public static Task<string> GetSampleResourceAsStringAsync(string fileName)
+        {
+            var resourceName = string.Join(".", typeof(TestSamples).Namespace, "Samples", fileName);
+            using var st = typeof(TestSamples).Assembly.GetManifestResourceStream(resourceName);
+            using var reader = new StreamReader(st);
+            return reader.ReadToEndAsync();
+        }
+
+        public static Task<string> GetErrorAsync() => GetSampleResourceAsStringAsync("error.xml");
+        public static Task<string> GetSimListResponseAsync() => GetSampleResourceAsStringAsync("simListResponse.xml");
+    }
+}
--- test/ThingsMobile.Tests/ThingsMobileClientTests.cs
+++ test/ThingsMobile.Tests/ThingsMobileClientTests.cs
@@ -2,60 +2,12 @@ using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using ThingsMobile;
-using ThingsMobile.Tests;
 using Xunit;
 
-namespace productboard.Tests
+namespace ThingsMobile.Tests
 {
     public class ThingsMobileClientTests
     {
-        private const string OkResponse = "<result>" +
-                                                    "<done>true</done>" +
-                                                    "<sims>" +
-                                                        "<sim>" +
-                                                            "<activationDate>2018-02-07 15:52:49</activationDate>" +
-                                                            "<balance>30</balance>" +
-                                                            "<blockSimAfterExpirationDate>1</blockSimAfterExpirationDate>" +
-                                                            "<blockSimDaily>1</blockSimDaily>" +
-                                                            "<blockSimMonthly>1</blockSimMonthly>" +
-                                                            "<blockSimTotal>1</blockSimTotal>" +
-                                                            "<dailyTraffic>123</dailyTraffic>" +
-                                                            "<dailyTrafficThreshold>1000000</dailyTrafficThreshold>" +
-                                                            "<expirationDate>2018-02-20 00:00:00</expirationDate>" +
-                                                            "<iccid>8944501312167518236</iccid>" +
-                                                            "<lastConnectionDate></lastConnectionDate>" +
-                                                            "<monthlyTraffic>1234</monthlyTraffic>" +
-                                                            "<monthlyTrafficThreshold>2000000</monthlyTrafficThreshold>" +
-                                                            "<msisdn>447937557899</msisdn>" +
-                                                            "<name>name</name>" +
-                                                            "<plan>default</plan>" +
-                                                            "<status>active</status>" +
-                                                            "<tag>tag</tag>" +
-                                                            "<totalTraffic>0</totalTraffic>" +
-                                                            "<totalTrafficThreshold>3000000</totalTrafficThreshold>" +
-                                                            "<type>AllInOne Sim</type>" +
-                                                            "<cdrs>" +
-                                                            "<cdr>" +
-                                                            "<cdrImsi>123456</cdrImsi>" +
-                                                            "<cdrDateStart>2017-01-20 12:45:00</cdrDateStart>" +
-                                                            "<cdrDateStop>2017-01-20 12:50:00</cdrDateStop>" +
-                                                            "<cdrNetwork>Zone 1</cdrNetwork>" +
-                                                            "<cdrCountry>Zone 1</cdrCountry>" +
-                                                            "<cdrTraffic>123456</cdrTraffic>" +
-                                                            "<cdrOperator> ITAWI</cdrOperator>" +
-                                                            "</cdr>" +
-                                                            "</cdrs>" +
-                                                        "</sim>" +
-                                                    "</sims>" +
-                                                "</result>";
-
-        private const string ErrorResponse = "<result>" +
-                                                "<done>false</done>" +
-                                                "<errorCode>1</errorCode>" +
-                                                "<errorMessage>Error description</errorMessage>" +
-                                            "</result>";
-
         [Fact]
         public async Task RequestIsSerializedCorrectly()
         {
@@ -81,11 +33,11 @@ namespace productboard.Tests
                 var expectedBody = $"username={username}&token={token}";
                 var actualBody = await req.Content.ReadAsStringAsync();
                 Assert.Equal(expectedBody, actualBody);
-                
+
                 return new HttpResponseMessage
                 {
                     StatusCode = HttpStatusCode.OK,
-                    Content = new StringContent(OkResponse, Encoding.UTF8, "text/xml")
+                    Content = new StringContent(await TestSamples.GetSimListResponseAsync(), Encoding.UTF8, "text/xml")
                 };
             });
             var httpClient = new HttpClient(handler);
@@ -136,7 +88,7 @@ namespace productboard.Tests
                 return new HttpResponseMessage
                 {
                     StatusCode = HttpStatusCode.BadRequest,
-                    Content = new StringContent(ErrorResponse, Encoding.UTF8, "text/xml")
+                    Content = new StringContent(await TestSamples.GetErrorAsync(), Encoding.UTF8, "text/xml")
                 };
             });
             var httpClient = new HttpClient(handler);
