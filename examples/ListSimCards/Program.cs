e0af6eb7eb6955991a8fd749ed846616e8019103
Maxwell Weru <mburumaxwell@gmail.com>
2021-11-24 09:15:42 +0300

    Use top level statements

--- examples/ListSimCards/Program.cs
+++ examples/ListSimCards/Program.cs
@@ -1,35 +1,25 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Threading.Tasks;
 using ThingsMobile;
 
-namespace ListSimCards
+Console.WriteLine("Hello World!");
+
+var username = "your-username-here";
+var token = "your-token-here";
+
+var services = new ServiceCollection();
+services.AddThingsMobile(username: username, token: token);
+
+var provider = services.BuildServiceProvider();
+
+var client = provider.GetRequiredService<ThingsMobileClient>();
+
+var response = await client.GetSimCardsLiteAsync();
+var simcards = response.Resource;
+if (simcards is not null && simcards.Sims is not null)
 {
-    class Program
+    foreach (var sim in simcards.Sims)
     {
-        static async Task Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-
-            var username = "your-username-here";
-            var token = "your-token-here";
-
-            var services = new ServiceCollection();
-            services.AddThingsMobile(username: username, token: token);
-
-            var provider = services.BuildServiceProvider();
-
-            var client = provider.GetRequiredService<ThingsMobileClient>();
-
-            var response = await client.GetSimCardsLiteAsync();
-            var simcards = response.Resource;
-            if (simcards is not null && simcards.Sims is not null)
-            {
-                foreach (var sim in simcards.Sims)
-                {
-                    Console.WriteLine($"MSISDN: {sim.Msisdn}, Name: {sim.Name}, Tag: {sim.Tag}, Status: {sim.Status}");
-                }
-            }
-        }
+        Console.WriteLine($"MSISDN: {sim.Msisdn}, Name: {sim.Name}, Tag: {sim.Tag}, Status: {sim.Status}");
     }
 }
