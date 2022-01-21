995d7ad60cbf0c4fcb0853f5d56f00121260fca6
Dennis Geldhof <github@geldhof.eu>
2022-01-21 22:03:59 +0100

    Add constructors to fix the build of the example

--- src/ThingsMobile/ThingsMobileClient.cs
+++ src/ThingsMobile/ThingsMobileClient.cs
@@ -18,9 +18,24 @@ public class ThingsMobileClient
     /// <summary>
     /// Creates an instance if <see cref="ThingsMobileClient"/>
     /// </summary>
+    /// <param name="options">The options for configuring the client</param>
+    public ThingsMobileClient(ThingsMobileClientOptions options)
+        : this(null, options) { }
+
+    /// <summary>
+    /// Creates an instance if <see cref="ThingsMobileClient"/>
+    /// </summary>
+    /// <param name="httpClient"></param>
+    /// <param name="options">The options for configuring the client</param>
+    public ThingsMobileClient(HttpClient? httpClient, ThingsMobileClientOptions options)
+        : this(httpClient, Options.Create(options)) { }
+
+    /// <summary>
+    /// Creates an instance if <see cref="ThingsMobileClient"/>
+    /// </summary>
     /// <param name="httpClient"></param>
     /// <param name="optionsAccessor">The options for configuring the client</param>
-    public ThingsMobileClient(HttpClient httpClient, IOptions<ThingsMobileClientOptions> optionsAccessor)
+    public ThingsMobileClient(HttpClient? httpClient, IOptions<ThingsMobileClientOptions> optionsAccessor)
     {
         this.httpClient = httpClient ?? new HttpClient();
         options = optionsAccessor?.Value ?? throw new ArgumentNullException(nameof(optionsAccessor));
